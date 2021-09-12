9acde9529770a7ca4d401dc770368c1fcce3e8f1
pedrinhofss <pedrinhofss2010@gmail.com>
2021-09-12 11:14:41 -0300

    variables

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -4,11 +4,9 @@
 // About this `I AM NOT DONE` thing:
 // We sometimes encourage you to keep trying things on a given exercise,
 // even after you already figured it out. If you got everything working and
-// feel ready for the next exercise, remove the `I AM NOT DONE` comment below.
-
-// I AM NOT DONE
+// feel ready for the next exercise, remove the `I AM NOT DONE` comment below
 
 fn main() {
-    x = 5;
+    let x = 5;
     println!("x has the value {}", x);
 }
